b887a607da817935137a8c09790a4e4c2d92e366
Palani <saipabi123@gmail.com>
2023-12-12 23:35:15 +0530

    first commit

---
+++ task 9 WETamil Batch 52/index.js
@@ -0,0 +1,79 @@
+
+
+    let count = 11;
+    // document.querySelectorAll("p").innerHTML="Happy Independence Day";
+    
+    setTimeout(() => {
+        count--;
+        console.log(count);
+        setTimeout(() => {
+        count--;
+        console.log(count);
+        setTimeout(() => {
+        count--;
+        console.log(count);
+        setTimeout(() => {
+        count--;
+        console.log(count);
+        setTimeout(() => {
+        count--;
+        console.log(count);
+        setTimeout(() => {
+        count--;
+        console.log(count);
+        setTimeout(() => {
+        count--;
+        console.log(count);
+        setTimeout(() => {
+        count--;
+        console.log(count);
+        setTimeout(() => {
+        count--;
+        console.log(count);
+        setTimeout(() => {
+        count--;
+        console.log(count);
+        console.log("Happy Independence Day");
+        
+
+    }, 1000
+    );
+}, 1000
+    );
+}, 1000
+    );
+}, 1000
+    );
+}, 1000
+    );
+}, 1000
+    );
+}, 1000
+    );
+}, 1000
+    );
+}, 1000
+    );
+}, 1000
+    );
+
+
+
+
+// const stopTimeout = () => {
+//      countdown(timeoutId);
+//  };
+   
+
+
+    
+//         let timeoutId = null;
+//         timeoutId = setTimeout((
+//             document.querySelectorAll("p").innerHTML="Happy Independence Day";
+
+//         }, 10000
+//         );
+
+//         const setTimeout = () => {
+//             clearTimeout(timeoutId);
+//         };
